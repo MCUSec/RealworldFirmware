3f76ebd60340ca3c8374968c28f7816aa91e84b6
Nicolas Nino <nfn57152@uga.edu>
2024-07-23 11:13:07 -0400

    Running FirmProcessing's step1 with sudo

--- FirmProcessing/pipeline.sh
+++ FirmProcessing/pipeline.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Running step 1"
-# If current user is not root run with sudo  
-python3 run_step1_convert2bin.py
+sudo python3 run_step1_convert2bin.py
 
 echo "Running step 2"
 python3 run_step2_binsorter.py --enable-firmxray
--- crawler/run.sh
+++ crawler/run.sh
@@ -16,4 +16,13 @@ python3 run_scrapy.py
 cd ..
 
 # Move files to FirmProcessing
-cp -r httpftp/results/files/* ../../FirmProcessing/originals
+#cp -r httpftp/results/files/* ../../FirmProcessing/originals
+
+
+if [ "$(ls -A results/files)" ]; then
+    echo "Files found, proceeding with copy."
+    # Move files to FirmProcessing
+    cp -r results/files/* ../../FirmProcessing/originals
+else
+    echo "No files found, skipping copy."
+fi
